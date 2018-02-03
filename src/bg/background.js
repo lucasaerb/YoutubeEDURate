48edcd1dd337d0fb4db9da181307075eceb2eb9a
lucasaerb <lucasaerb@gmail.com>
2018-02-03 00:32:28 -0800

    Added functionality to scrape the web.

--- src/bg/background.js
+++ src/bg/background.js
@@ -6,6 +6,28 @@
 
 
 //example of using a message handler from the inject scripts
+
+chrome.runtime.onMessage.addListener(function(request, sender) {
+  if (request.action == "getSource") {
+    message.innerText = request.source;
+  }
+} );
+
+function onWindowLoad() {
+
+  var message = document.querySelector('#message');
+
+  chrome.tabs.executeScript(null, {
+    file: "getPagesSource.js"
+  }, function() {
+    // If you try and inject into an extensions page or the webstore/NTP you'll get an error
+    if (chrome.runtime.lastError) {
+      message.innerText = 'There was an error injecting script : \n' + chrome.runtime.lastError.message;
+    }
+  });
+
+}
+
 chrome.extension.onMessage.addListener(
   function(request, sender, sendResponse) {
   	chrome.pageAction.show(sender.tab.id);
