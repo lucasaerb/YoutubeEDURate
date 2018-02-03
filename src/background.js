099d93fcf62adfaf1ba7d3802e8cfea8d664aa14
Nisarg Shah <npshah9856@gmail.com>
2018-02-03 09:21:32 -0800

    Updating content.js and background.js, added button clicked functionality

--- src/background.js
+++ src/background.js
@@ -1,1 +1,10 @@
-console.log("In background");
+console.log("In background!");
+chrome.browserAction.onClicked.addListener(buttonClicked);
+
+function buttonClicked(tab){
+    console.log(tab);
+    let msg = {
+      "txt" : "activate"
+    }
+    chrome.tabs.sendMessage(tab.id,msg);
+}
--- src/content.js
+++ src/content.js
@@ -1,10 +1,19 @@
 console.log("In the extension!!");
 
-let a_tags = document.getElementsByTagName('a')
+function getTitles(){
+  let a_tags = document.getElementsByTagName('a')
+  for(elm of a_tags){
+    if (elm.id == "video-title"){
+      console.log(elm.title);
+}
 
-//let titles = a_tags.getElementById('title');
-for(elm of a_tags){
-  if (elm.id == "video-title"){
-    console.log(elm.title);
+chrome.runtime.onMessage.addListener(message);
+
+function message(msg,sender,sendResponse){
+  console.log(msg.txt);
+  if(msg.txt === "activate"){
+      getTitles();
+      }
+    }
   }
 }
