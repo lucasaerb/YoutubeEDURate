5243dfb80c02cd68bb74253bd6ea5371c486a650
Lucas Erb <lucasaerb@gmail.com>
2018-02-04 00:37:11 -0800

    Unnecessary Rating File.

--- backend/rate.py
+++
@@ -1,8 +0,0 @@
-
-def getRate(title):
-    counter = 0
-    split = title.rstrip().split()
-    for i in split:
-        if i in open("wordList.txt").read():
-            counter+=1
-    return counter/len(split)
